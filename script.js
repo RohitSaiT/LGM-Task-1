ded78585809a1c4e4e1450da45fe1599971b8723
RohitSaiT <88829660+RohitSaiT@users.noreply.github.com>
2021-09-24 22:52:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,20 +14,4 @@ $('.slider-single').slick({
 
 
 
-// Google Map
 
-function initMap() {
-  // The location of Uluru
-  const VIT = { lat: 12.9717, lng: 79.1594 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: VIT,
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: VIT,
-    map: map,
-  });
-}
